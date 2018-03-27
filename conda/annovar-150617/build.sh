200faf16e1bdbd4c78c8d63b5449102aa93210b5
Stephen Kelly <stevekm@users.noreply.github.com>
2018-03-27 18:27:31 -0400

    update Makefile and configs for conda

--- conda/annovar-150617/build.sh
+++ conda/annovar-150617/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -x -e
-wget http://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.revision150617.tar.gz
-tar xvfz annovar.revision150617.tar.gz
-rm -f annovar.revision150617.tar.gz
+echo $PWD
+ls -l 
